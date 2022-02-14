b0143b425a0f6173f07d8daef003ff14e55ba513
barinale <barinale08@gmail.com>
2022-02-14 16:43:41 +0000

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,133 @@
+
+class Slider{
+    constructor(Arrow,items,progress=null){
+        this.LeftArrow = Arrow.querySelector('.rightArrow');
+        this.RightArrow = Arrow.querySelector('.leftArrow');   
+        this.items = items;
+        console.log(this.LeftArrow)
+        console.log(this.RightArrow) 
+        this.merge = this.items.getBoundingClientRect().x;
+        this.postion = 0;
+        this.addEvent()
+        this.item = this.items.querySelectorAll('.item');
+        this.postions = this.allPostion();
+        if(progress!=null){
+        this.progress = progress;
+        this.move()
+        this.width = 8;
+        }
+        this.click= false;
+        console.log(this.merge)
+    }
+    addEvent(){
+        this.LeftArrow.addEventListener('click',()=>{
+            if(this.progress!=null){
+            this.width=8;
+            this.check();
+            this.click = true;}
+            this.LeftMove()
+        
+        })
+        this.RightArrow.addEventListener('click',()=>{
+            if(this.progress!=null){
+            this.width=8;
+            this.check();
+            this.click = true;
+        }
+            this.rightMove()})
+    }
+    allPostion(){
+
+      let Array =[];
+      this.item.forEach(element => {
+          Array.push(element.getBoundingClientRect().x-this.merge)
+          console.log(this.merge)
+      });
+      console.log(Array)
+      return Array;
+    }
+    LeftMove(){
+        console.log("erere")
+        if(this.postion==this.postions.length-1){
+            this.postion=0;
+        this.items.style.right=`${this.postions[this.postion]}px`;
+
+        }else{
+        this.postion++;
+        this.items.style.right=`${this.postions[this.postion]}px`;
+        }
+    }
+    rightMove(){
+        if(this.postion==0){
+            this.postion=this.postions.length-1;
+          this.items.style.right=`${this.postions[this.postion]}px`;
+        }else{
+        this.postion--;
+        this.items.style.right=`${this.postions[this.postion]}px`;
+        
+        }
+    }
+    resize(){
+        this.postions = this.allPostion()
+        this.click = true;
+               this.item = this.items.querySelectorAll('.item');
+
+        if(this.progress!=null){
+            this.check()
+            this.width=8;
+        }
+          this.items.style.right=`0px`;
+
+    }
+    move(){
+        
+            setInterval(() => {
+                this.progress.style.width = this.width+'%';
+
+                    if(!this.click){
+                    this.progress.style.width = this.width+'%';
+                    this.width++;
+                    if(this.width>=100){
+                        this.width=0;
+                        this.LeftMove()
+                    }
+            }
+            }, 70);
+        
+    }
+    check(){
+        setTimeout(() => {
+            this.click=false
+        }, 3000);
+     
+    }
+
+}
+
+const SliderMain = new Slider(document.querySelector('#main .Arrows'),
+                                document.querySelector('#main .items'),
+                                document.querySelector('#main .progress'));
+
+const SliderMain1 = new Slider(document.querySelector('#OeuvreMajeur .Arrows'),
+                            document.querySelector('#OeuvreMajeur .items')
+                            ,null);
+
+
+const SliderMain2 = new Slider(document.querySelector('#calendrier .Arrow'),
+                            document.querySelector('#calendrier .items')
+                            ,null);
+
+
+const SliderMain3 = new Slider(document.querySelector('#ventreRec .OeuvreArrow'),
+                            document.querySelector('#ventreRec .items')
+                            ,null);
+
+window.addEventListener('resize', ()=>{
+    SliderMain.resize()
+    SliderMain1.resize()
+    SliderMain2.resize()
+    SliderMain3.resize()
+
+});
+
+
